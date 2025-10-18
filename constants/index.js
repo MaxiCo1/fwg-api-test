d6e9934138566a995c491ab9799c6b1d22b1b77f
Maximiliano Coletti <coletti.maxi@gmail.com>
2025-10-17 22:19:59 -0300

    new test

---
+++ constants/index.js
@@ -0,0 +1,5 @@
+const SPREADSHEETS = {
+  WEBSITE_SETUP_IONOS: process.env.SPREADSHEET_ID
+};
+
+module.exports = { SPREADSHEETS };
---
+++ google-auth/noreply-google-auth.js
@@ -0,0 +1,15 @@
+const { google } = require("googleapis");
+
+async function authorize() {
+  const auth = new google.auth.GoogleAuth({
+    credentials: {
+      client_email: process.env.GOOGLE_CLIENT_EMAIL,
+      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n")
+    },
+    scopes: ["https://www.googleapis.com/auth/spreadsheets"]
+  });
+
+  return await auth.getClient();
+}
+
+module.exports = { authorize };
